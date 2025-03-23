de23a2fd8043aa310a07308bebfaa2782c9d6fba
ayaka <2282514478@qq.com>
2025-03-23 19:55:23 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
